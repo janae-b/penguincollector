7c2fc0370d4e46edaf58249de63e2f2f092c0ed5
janae-b <janae.boersma@comcast.net>
2021-05-04 23:02:16 -0500

    up to lab part 5

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Penguin
+from .models import Penguin, Feeding, Toy
 
 # Register your models here.
 admin.site.register(Penguin)
+admin.site.register(Feeding)
+admin.site.register(Toy)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Penguin
+from django.views.generic import ListView, DetailView
+from .models import Penguin, Toy
+from .forms import FeedingForm
 
 def home(request):
   return render(request, 'home.html')
@@ -14,7 +16,39 @@ def penguins_index(request):
 
 def penguins_detail(request, penguin_id):
   penguin = Penguin.objects.get(id=penguin_id)
-  return render(request, 'penguins/detail.html', { 'penguin': penguin })
+  toys_penguin_doesnt_have = Toy.objects.exclude(id__in = penguin.toys.all().values_list('id'))
+  feeding_form = FeedingForm()
+  return render(request, 'penguins/detail.html', {
+    'penguin': penguin, 'feeding_form': feeding_form,
+    # Add the toys to be displayed
+    'toys': toys_penguin_doesnt_have
+  })
+
+def add_feeding(request, penguin_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.penguin_id = penguin_id
+    new_feeding.save()
+  return redirect('detail', penguin_id=penguin_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
 
 class PenguinCreate(CreateView):
   model = Penguin
@@ -28,3 +62,7 @@ class PenguinUpdate(UpdateView):
 class PenguinDelete(DeleteView):
   model = Penguin
   success_url = '/penguins/'
+
+def assoc_toy(request, penguin_id, toy_id):
+  Penguin.objects.get(id=penguin_id).toys.add(toy_id)
+  return redirect('detail', penguin_id=penguin_id)
